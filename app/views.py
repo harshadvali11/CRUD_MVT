493867874b309e4787e5692287e0d22a2194e35f
harshadvali11 <harshadvali1431@gmail.com>
2022-12-15 18:27:06 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -26,6 +26,7 @@ def display_webpages(request):
     LWO=Webpage.objects.filter(Q(topic_name='Cricket') & Q(name__startswith='m'))
     LWO=Webpage.objects.all()
     LWO=Webpage.objects.filter(Q(topic_name='Foot Ball') | Q(url__endswith='in'))
+    LWO=Webpage.objects.all()
     d={'LWO':LWO}
     return render(request,'display_webpages.html',d)
 
@@ -51,7 +52,24 @@ def display_access(request):
     d={'LAO':LAO}
     return render(request,'display_access.html',d)
 
+def update_webpage(request):
+    #Webpage.objects.filter(topic_name='Boxing').update(name='Naresh',url='https://Naresh.in')
+    #Webpage.objects.filter(name='ABCDE').update(topic_name='Foot Ball')
+    T=Topic.objects.get_or_create(topic_name='Cricket')[0]
+    T.save()
+    Webpage.objects.update_or_create(name='ABD',defaults={'topic_name':T,'url':'https://ABD.in'})
+    LWO=Webpage.objects.all()
+    d={'LWO':LWO}
+    return render(request,'display_webpages.html',d)
 
+
+def delete_webpage(request):
+    #Webpage.objects.filter(topic_name='Cricket').delete()
+    
+    Webpage.objects.all().delete()
+    LWO=Webpage.objects.all()
+    d={'LWO':LWO}
+    return render(request,'display_webpages.html',d)
 
 
 
